d086cd0c2106f3057118ee58a72dee0aa83e16f2
tesserato <tesserato@hotmail.com>
2020-06-06 20:16:58 -0300

    switched to named constraints

--- Python/Explorations/MILP/MILP.py
+++ Python/Explorations/MILP/MILP.py
@@ -3,19 +3,20 @@ import random
 import numpy as np
 
 #######################
-n=40
-random.seed(0)
+random.seed(2)
+n=10
+
 X = np.arange(n)
 W = np.zeros(n)
 number_of_random_waves = 1
 A = np.array([random.uniform(1, 5) for i in range(number_of_random_waves)])
-F = np.array([random.uniform(1, 9) for i in range(number_of_random_waves)])
+F = np.array([random.uniform(1, n/2) for i in range(number_of_random_waves)])
 P = np.array([random.uniform(0, 2 * np.pi) for i in range(number_of_random_waves)])
 W = np.sum(A * np.cos(P + 2 * np.pi * F.T * X[:, np.newaxis] / n), 1)
 W = W / np.max(np.abs(W))
 idx = np.argmax(A)
-print(W)
-print(F[idx])
+# print(W)
+print(f"f={round(F[idx], 2)} p={round(P[idx], 2)}")
 #######################
 
 #######################
@@ -37,18 +38,17 @@ for x in range(n):
   den = np.sqrt(n**2 + 4 * np.pi**2 * x**2)
   m = 2 * np.pi * n / den
   d = (n * p + 2 * np.pi * x * f) / den
-  k = pulp.LpVariable(f"k_{x}", 0, cat=pulp.LpInteger)
-  a = pulp.LpVariable(f"a_{x}", 0)
+  k = pulp.LpVariable(f"k_{x}", 0, x+1, cat=pulp.LpInteger)
+  a = pulp.LpVariable(f"a_{x}", 0, x+1)
   if W[x] >= 0:
     r = d - k * m
   else:
     r = d - k * m + m / 2
-  prob += a >= r
-  prob += a >= - r
+  prob += pulp.LpConstraint(a - r, rhs=0 , sense=pulp.LpConstraintGE, name=f"c_{x}_pos") # a >= r
+  prob += pulp.LpConstraint(a + r, rhs=0 , sense=pulp.LpConstraintLE, name=f"c_{x}_neg") # a >= - r
   O.append(a)
 
 prob += pulp.lpSum([O[x] * abs(W[x]) for x in range(n)]) # <|<|<|<|<|<|<|<|
-
 
 prob.writeLP("lp.lp")
 
@@ -58,8 +58,8 @@ status = prob.solve(pulp.CPLEX_CMD(path=r"C:\Program Files\IBM\ILOG\CPLEX_Studio
 
 print(pulp.LpStatus[status])
 
-for v in prob.variables():
-  print(v.name, "=", v.varValue)
+# for v in prob.variables():
+#   print(v.name, "=", v.varValue)
 
 print(f"f={round(pulp.value(f), 2)} p={round(pulp.value(p), 2)}")
 
