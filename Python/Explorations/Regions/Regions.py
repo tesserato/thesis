0c2ce17e086f879f9f24f16b5a5f8081b01af19f
tesserato <tesserato@hotmail.com>
2020-06-12 13:50:00 -0300

    prototype seems ok

--- Python/Explorations/Regions/Regions.py
+++ Python/Explorations/Regions/Regions.py
@@ -2,8 +2,16 @@ import numpy as np
 import plotly.graph_objects as go
 import random
 
+class interval:
+  def __init__(self, start, end, value=0):
+    self.start = start
+    self.end = end
+    self.value = value
+
+
+
 ### Generating random wave
-n = 10
+n = 20
 random.seed(0)
 X = np.arange(n)
 W = np.zeros(n)
@@ -14,6 +22,8 @@ P = np.array([random.uniform(0, 2 * np.pi) for i in range(number_of_random_waves
 W = np.sum(A * np.cos(P + 2 * np.pi * F.T * X[:, np.newaxis] / n), 1)
 W = W / np.max(np.abs(W))
 idx = np.argmax(A)
+
+print(F[idx])
 
 fig = go.Figure()
 
@@ -37,7 +47,7 @@ fig.update_layout(
 fig.add_trace(
   go.Scatter(
     x=[0, 0],
-    y=[-n, n],
+    y=[0, n//2+1],
     hoverinfo=f"all",
     name=f"t={0}, a={W[0]:.2f}", 
     mode='lines',
@@ -48,9 +58,8 @@ fig.add_trace(
   )
 )
 
-regions0 = [[-n, n, 1 if W[0] >= 0 else 0]]
-regions1 = []
-x0 = 1
+x0 = 0
+regions = [] # list of [start, end, weight] #[[0, 1 if W[0] >= 0 else 0], [n//2+1, 1 if W[0] >= 0 else 0]]
 yticks = []
 for x in range(1, n // 2 + 1):
 
@@ -59,26 +68,16 @@ for x in range(1, n // 2 + 1):
   X_lines_o = []
   Y_lines_o = []
   
-  w = 1 if W[x] >= 0 else 0
-  regions1 = []
+  w = 1 if W[x] >= 0 else -1
+
   for k in range(x // 2 + 1):
     f0_e = np.round((4 * k - 1) * n / (4 * x), 2)
     f1_e = np.round((4 * k + 1) * n / (4 * x), 2)
-
+    regions.append(interval(f0_e, f1_e, w))
     f0_o = np.round((4 * k + 1) * n / (4 * x), 2)
     f1_o = np.round((4 * k + 3) * n / (4 * x), 2)
-
-    for r in regions0:
-      if r[0] < f0_e:
-        if r[1] > f1_e: # r contains f
-          print("r contains fe")
-          regions1.append([f0_e, f1_e, r[2] + 1])
-          # regions1.append([f1_e, r[1], 1])
-        # else: # r1 < f1
-        #   print("r[0] < f0 and r1 < f1")
-        #   regions1.append([r[0], f0_e, 1])
-        #   regions1.append([f0_e, r[1], r[2] + 1])
-        #   regions1.append([r[1], f1_e, r[2] + 1])   
+    regions.append(interval(f0_o, f1_o, -w))
+    yticks.append(f0_e), yticks.append(f1_e), yticks.append(f0_o), yticks.append(f1_o)
 
     X_lines_e.append(x0)
     X_lines_e.append(x0)
@@ -93,24 +92,7 @@ for x in range(1, n // 2 + 1):
     Y_lines_o.append(f0_o)
     Y_lines_o.append(f1_o)
     Y_lines_o.append(None)
-    # yticks.append(f0)
-    # yticks.append(f1)
-    
-  for l in regions1:
-    fig.add_trace(
-      go.Scatter(
-        x=[.5, .5],
-        y=[l[0], l[1]],
-        hoverinfo=f"all",
-        name=f"t={x}, a={l[2]:.2f}", 
-        mode='lines',
-        line=go.scatter.Line(
-          # color="black",
-          width=10#max(1, W[x]**2 * 10)
-        )
-      )
-    )
-  regions0[:] = regions1[:]
+
   x0 +=1
 
   fig.add_trace(
@@ -141,9 +123,31 @@ for x in range(1, n // 2 + 1):
     )
   )
 
-# print(len(yticks))
-# yticks = np.unique(np.array(yticks))
-# print(yticks.shape)
+print(len(yticks))
+yticks = np.unique(np.array(yticks))
+print(yticks.shape)
+
+summation = [interval(yticks[i], yticks[i + 1], 0) for i in range(len(yticks) - 1)]
+
+for r in regions:
+  for s in summation:
+    if r.start <= s.start and r.end >= s.end:
+      s.value += r.value
+
+for s in summation:
+  fig.add_trace(
+    go.Scatter(
+      x=[x0, x0],
+      y=[s.start, s.end],
+      hoverinfo=f"all",
+      name=f"t={x}, a={s.value:.2f}", 
+      mode='lines',
+      line=go.scatter.Line(
+        color="red",
+        width= abs(s.value) * 5
+      )
+    )
+  )
 
 fig.update_yaxes(tickvals=yticks)
 
