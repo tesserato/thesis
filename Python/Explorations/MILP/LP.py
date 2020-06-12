2d6ddecc3de1b32064cb7598d6e12ab3ac756c1b
tesserato <tesserato@hotmail.com>
2020-06-12 09:40:59 -0300

    started working on LP version

--- Python/Explorations/MILP/LP.py
+++ Python/Explorations/MILP/LP.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 #######################
-random.seed(2)
+random.seed(0)
 n=10
 
 X = np.arange(n)
@@ -33,21 +33,28 @@ p = pulp.LpVariable("p", 0 , 2 * np.pi)
 f = pulp.LpVariable("f", 0 , n / 2)
 
 O = [] # opt
-for x in range(n):
-  den = np.sqrt(n**2 + 4 * np.pi**2 * x**2)
-  m = 2 * np.pi * n / den
-  d = (n * p + 2 * np.pi * x * f) / den
-  k = pulp.LpVariable(f"k_{x}", 0, x+1, cat=pulp.LpInteger)
-  a = pulp.LpVariable(f"a_{x}", 0, x+1)
-  if W[x] >= 0:
-    r = d - k * m
-  else:
-    r = d - k * m + m / 2
-  prob += pulp.LpConstraint(a - r, rhs=0 , sense=pulp.LpConstraintGE, name=f"c_{x}_pos") # a >= r
-  prob += pulp.LpConstraint(a + r, rhs=0 , sense=pulp.LpConstraintLE, name=f"c_{x}_neg") # a >= - r
-  O.append(a)
 
-prob += pulp.lpSum([O[x] * abs(W[x]) for x in range(n)]) # <|<|<|<|<|<|<|<|
+for x in range(n):
+  A = []
+  for k in range(x+1):    
+    a = pulp.LpVariable(f"a_{x}_{k}", 0)
+    b = pulp.LpVariable(f"b_{x}_{k}", 0)
+    den = np.sqrt(n**2 + 4 * np.pi**2 * x**2)
+    if W[x] >= 0:
+      d = (n * p + 2 * np.pi * x * f - 4 * np.pi**2 * k * x) / den
+    else:
+      d = (n * p + 2 * np.pi * x * f - 4 * np.pi**2 * k * x - np.pi * n) / den
+
+    prob += a >= + d # pulp.LpConstraint(a - d, rhs=0 , sense=pulp.LpConstraintGE, name=f"c_{x}_{k}_pos") #
+    prob += a >= - d # pulp.LpConstraint(a + d, rhs=0 , sense=pulp.LpConstraintLE, name=f"c_{x}_{k}_neg") #
+    
+    O.append(a * abs(W[x]))
+    A.append(d)
+  # prob += pulp.Lpsum([a for a in O]) == pulp. ([a for a in O])
+  # prob += pulp.LpConstraint(pulp.lpSum([a for a in O]), rhs=1 ,sense=pulp.LpConstraintGE)
+
+
+prob += pulp.lpSum([a for a in O]) # <|<|<|<|<|<|<|<|
 
 prob.writeLP("lp.lp")
 
@@ -57,8 +64,8 @@ status = prob.solve(pulp.CPLEX_CMD(path=r"C:\Program Files\IBM\ILOG\CPLEX_Studio
 
 print(pulp.LpStatus[status])
 
-# for v in prob.variables():
-#   print(v.name, "=", v.varValue)
+for v in prob.variables():
+  print(v.name, "=", v.varValue)
 
 print(f"f={round(pulp.value(f), 2)} p={round(pulp.value(p), 2)}")
 
