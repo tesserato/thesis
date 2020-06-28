145f7991825d3646dc8e72d9b738fcd53d321745
tesserato <tesserato@hotmail.com>
2020-06-28 13:20:42 -0300

    started cycles based approach

--- Python/Make wav.py
+++ Python/Make wav.py
@@ -4,7 +4,7 @@ import numpy as np
 from Helper import save_wav
 
 random.seed(0)
-n = 1000
+n = 10000
 
 X = np.arange(n)
 Y = np.zeros(n)
@@ -14,4 +14,4 @@ p = np.pi
 Y = np.cos(p + 2 * np.pi * f * X / n)
 
 
-save_wav(5000 * Y, f"Python/local_f={np.round(f, 2)}-p={np.round(p, 2)}-n={n}.wav")
+save_wav(5000 * Y, f"Samples/local_f={np.round(f, 2)}-p={np.round(p, 2)}-n={n}.wav")
