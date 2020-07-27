cbeff1c28a1e2da09c5e1e5e0346f3cfdbcd5ed6
tesserato <tesserato@hotmail.com>
2020-07-27 16:41:39 -0300

    algorithm working, but needs improvement

--- Python/AlphaShapes.py
+++ Python/AlphaShapes.py
@@ -1,9 +1,13 @@
 import plotly.graph_objects as go
 from scipy.interpolate import interp1d, UnivariateSpline
+from scipy.linalg import block_diag
+from scipy.optimize import nnls
 import numpy as np
 from Helper import read_wav
 from scipy.signal import savgol_filter
 import numpy.polynomial.polynomial as poly
+import pulp
+
 
 class Pulse:
   def __init__(self, x0, W, is_noise = False):
@@ -67,82 +71,235 @@ def get_pulses_area(pulses):
   return X, Y
 
 
-def get_average_curvature(X, Y):
+def bezier_3_points(X, Y):
+  assert len(X) == len(Y) and len(X) == 3
+
+  # def line(x0, x1, y0, y1, t):
+  #   '''y = m x + b | 0 = x0, 1 = x1'''
+  #   x = x0 + t * (x1 - x0)
+  #   m = (y1 - y0) / (x1 - x0)
+  #   b = y0 - m * x0
+  #   f = lambda alpha : m * alpha + b
+  #   y = f(x)
+  #   return x, y
+
+  x0, x1, x2 = X[0], X[1], X[2]
+  # y0, y1, y2 = Y[0], Y[1], Y[2]
+
+  if x0 + x2 == 2 * x1:
+    print("oooops")
+    x2 = x2 + 0.001
+
+  def bez(x, x0=X[0], x1=X[1], x2=x2, y0=Y[0], y1=Y[1], y2=Y[2]):
+    a = x0 - x1
+    b = np.sqrt(x * x0 - 2 * x * x1 + x * x2 - x0 * x2 + x1**2)
+    c = x0 - 2 * x1 + x2
+    # print(">>>", a, b, c)
+    t = (a+b) / c
+    # xa, ya = line(x0, x1, y0, y1, t)
+    # xb, yb = line(x1, x2, y1, y2, t)
+    # x, y = line(xa, xb, ya, yb, t)
+    y = y0 * (1 - t)**2 + 2 * t * (1 - t) * y1 + y2 * t**2
+    return y
+
+  return bez
+
+
+def get_curvature(X, Y, n):
   '''Return average curvature and radius of the equivalent circle for the poly approximation of 4 points amplitude at a time'''
-  curvatures = []
-  for i in range(len(X) - 3):
-    x0, x1, x2, x3 = X[i], X[i + 1], X[i + 2], X[i + 3]
-    y0, y1, y2, y3 = Y[i], Y[i + 1], Y[i + 2], Y[i + 3]
-    x, y = np.array([x0, x1, x2, x3]), np.array([y0, y1, y2, y3])
-    a0, a1, a2 = poly.polyfit(x, y, 2)
-    c = 2 * a2 / ((a1 + a2 * (x1 + x2))**2 + 1)**(3/2)
-    curvatures.append(c)
-  average_curvature = np.abs(np.average(curvatures))
-  radius = 1 / average_curvature
-  return  average_curvature, radius
+  pos_curvatures_X = []
+  pos_curvatures_Y = []
+  neg_curvatures_X = []
+  neg_curvatures_Y = []
+  for i in range(2, len(X) - 2):
+    x0, x1, x2, x3, x4 = X[i - 2], X[i - 1], X[i], X[i + 1], X[i + 2]
+    y0, y1, y2, y3, y4 = Y[i - 1], Y[i - 1], Y[i], Y[i + 1], Y[i + 1]
+
+    yl1 = (y2 - y0) / (x2 - x0)
+    yl2 = (y3 - y1) / (x3 - x1)
+    yl3 = (y4 - y2) / (x4 - x2)
+
+    yll1 = (yl2 - yl1) / (x2 - x1)
+    yll2 = (yl3 - yl2) / (x3 - x2)
+
+    yl = (yl1 + yl2 + yl3) / 3
+    yll = (yll1 + yll2) / 2
+
+    c = yll / (1 + yl**2)**(3 / 2)
+    if c >= 0:
+      pos_curvatures_X.append(x2)
+      pos_curvatures_Y.append(c)
+    else:
+      neg_curvatures_X.append(x2)
+      neg_curvatures_Y.append(c)
+
+  if np.average(pos_curvatures_Y) < np.abs(np.average(neg_curvatures_Y)):
+    curvatures_X = pos_curvatures_X
+    curvatures_Y = pos_curvatures_Y
+  else:
+    curvatures_X = neg_curvatures_X
+    curvatures_Y = np.abs(neg_curvatures_Y)
+
+  i = 0
+  lim = (X[-1] - X[0]) / 3
+  while curvatures_X[i] < lim:
+    i += 1
+  p1 = i
+  while curvatures_X[i] < 2 * lim:
+    i += 1
+  p2 = i
+
+  x0 = 0
+  y0 = np.average(curvatures_Y[0 : p1])
+  x1 = (X[-1] - X[0]) / 2
+  y1 = np.average(curvatures_Y[p1 : p2])
+  x2 = n
+  y2 = np.average(curvatures_Y[p2 : ])
+
+  #########
+  # g = interp1d([x0, x1, x2], [y0, y1, y2], "quadratic")
+  # f = lambda x : 1 / g(x)
+  #########
+
+  # interp = bezier_3_points([x0, x1, x2], [y0, y1, y2])
+  
+  aa = ((x0 - x1) * (y1 - y2) - (x1 - x2) * (y0 - y1)) / (2 * (-x0 + x2) * (x0 - x1) * (x1 - x2))
+  bb = (-x0 * (x0 - x1) * (y1 - y2) + x2 * (x1 - x2) * (y0 - y1)) / ((-x0 + x2) * (x0 - x1) * (x1 - x2))
+  cc = -aa * x0**2 - bb * x0 + y0
+  # print("abc:",aa, bb, cc)
+  # print("xxx:",x0, x1, x2)
+  # print("yyy:",y0, y1, y2)
+
+  def g(x):
+    x = np.array(x, dtype=np.float64)
+    return (aa * x**2 + bb * x + cc)
+
+  def f(x):
+    x = np.array(x, dtype=np.float64)
+    return 1 / (aa * x**2 + bb * x + cc)
+
+  fig.add_trace(
+    go.Scatter(
+      name="curvatures",
+      x=curvatures_X,
+      y=curvatures_Y,
+      mode="markers",
+      marker=dict(
+          # size=8,
+          color="yellow",
+          # showscale=False
+      )
+    )
+  )
+
+  fig.add_trace(
+    go.Scatter(
+      name="curvature points",
+      x=[x0, x1, x2],
+      y=[y0, y1, y2],
+      mode="markers",
+      marker=dict(
+          # size=8,
+          color="pink",
+          # showscale=False
+      )
+    )
+  )
+
+  XX = np.arange(n)
+  np.savetxt("XX.csv", XX, delimiter=",")
+  fig.add_trace(
+    go.Scatter(
+      name="curvatures",
+      x=XX,
+      y=g(XX),
+      mode="lines",
+      line=dict(
+          # size=8,
+          color="green",
+          # showscale=False
+      )
+    )
+  )
+
+  return f
 
 
-def constrained_least_squares(X, Y, q=4, k=4):
+def constrained_least_squares_arbitrary_intervals(X, Y, I, k=3):
   '''constrained least squares with q intervals and k-1 polynomial'''
-  assert X.size == Y.size
-  n = X.size
-  l = n // q
-  U = np.zeros((n, k * q))
-  V = np.zeros((2 * (q-1), k * q))
+  assert len(X) == len(Y)
+  n = len(X)
+  I = [0] + I + [n]
 
-  '''populating U'''
-  for i1 in range(q):
-    for i2 in range(l * i1, l * (i1 + 1)):
-      for i3 in range(k):
-        U[i2, i1 * k + i3] = X[i2]**i3 #f"X_p[{i2}]**{i3}"
+  # U = np.zeros((n, k * q))
 
-  '''populating V'''
-  for i in range(q - 1):
-    for j in range(k):
-      V[2 * i, i * k + j] = X[(i + 1) * l]**j #f"X_p[({i + 1}) * l]**{j}"
-      V[2 * i, i * k + k + j] = -X[(i + 1) * l]**j #f"-X_p[({i + 1}) * l]**{j}"
-  for i in range(q - 1):
-    for j in range(1, k):
-      V[2 * i + 1, i * k + j] = j * X[(i + 1) * l] ** (j - 1) #f"{j} * X_p[({i + 1}) * l]**({j-1})"
-      V[2 * i + 1, i * k + k + j] = -j * X[(i + 1) * l] ** (j - 1) #f"{-j} * X_p[({i + 1}) * l]**({j-1})"
+  U = []
+
+  for i in range(len(I) - 1):
+    l0, l1 = I[i], I[i + 1]
+    Ui = np.zeros((l1 - l0, k + 1))
+    for j in range(l1 - l0):
+      for c in range(k + 1):
+        Ui[j, c] = X[l0 + j]**c
+
+    U.append(Ui)
+
+  U = block_diag(*U)
+
+  V = np.zeros((2 * (len(I) - 2), (k + 1) + (k + 1) * (len(I) - 2)))
+
+  for i in range(len(I) - 2):
+    V[2 * i, i * (k + 1)] = 1
+    V[2 * i, (i + 1) * (k + 1)] = -1
+    for c in range(1, k + 1):
+      V[2 * i, i * (k + 1) + c] = X[i+1]**c
+      V[2 * i + 1, i * (k + 1) + c] = X[i+1]**(c-1)
+      V[2 * i, (i + 1) * (k + 1) + c] = -X[i+1]**c
+      V[2 * i + 1, (i + 1) * (k + 1) + c] = -X[i+1]**(c-1)
+
+  np.savetxt("U.csv", np.round(U, 2), delimiter=",")
+  np.savetxt("V.csv", np.round(V, 2), delimiter=",")
 
   '''solving'''
-  UTUinv = np.linalg.inv(np.matmul(U.T, U))
-  tau = np.linalg.inv(np.matmul(np.matmul(V, UTUinv),V.T))
+  UTUinv = np.linalg.inv(U.T @ U)
+  # tau = np.linalg.inv(np.matmul(np.matmul(V, UTUinv),V.T))
+  print(V.shape, UTUinv.shape)
+  tau = np.linalg.inv(V @ UTUinv @ V.T)
   UTW = np.matmul(U.T, Y)
   par1 = np.matmul(np.matmul(V, UTUinv), UTW)
   A = np.matmul(UTUinv,UTW - np.matmul(np.matmul(V.T, tau),par1))
-  return np.reshape(A, (q, -1))
+  return np.reshape(A, (len(I) - 1, -1))
 
 
-def coefs_to_array(A, X): 
+def coefs_to_array_arbitrary_intervals(A, X, I, n):
   '''evaluates x E X from a coefficient matrix A'''
-  n = X.size
-  q = A.shape[0]
   k = A.shape[1]
-  l = n // q
-  Y = []
-  for i in range(q):
-    for x in range(i * l, (i + 1) * l):
-      y = 0
-      for j in range(k):
-        y += A[i, j] * X[x]**j
-      Y.append(y)
+  Y = np.zeros(n)
+  Xs = [0] + [int(round(X[i])) for i in I] + [n]
 
-  x = len(Y)
-  while x < n:
-    y = 0
-    for i in range(k):
-      y += A[q-1, i] * X[x]**i
-    Y.append(y)
-    x += 1
+  for i in range(len(Xs) - 1):
+    x0, x1 = Xs[i], Xs[i + 1]
+    for x in range(x0 , x1):
+      for c in range(k):
+        Y[x] += A[i, c] * x**c
+
+  # x = len(Y)
+  # while x < n:
+  #   y = 0
+  #   for i in range(k):
+  #     y += A[q-1, i] * X[x]**i
+  #   Y.append(y)
+  #   x += 1
   return Y
 
 
 def get_circle(x0, y0, x1, y1, r):
   '''returns center of circle that passes through two points'''
+  
   radsq = r * r
   q = np.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
+  # assert q <= 2 * r, f"shit"
 
   x3 = (x0 + x1) / 2
   y3 = (y0 + y1) / 2
@@ -181,15 +338,16 @@ def draw_circle(xc, yc, r, fig, n=100):
   return X, Y
 
 
-def get_frontier(X, Y):
-  '''extracts the envelope via rolling circle method'''
-  _, r = get_average_curvature(X, Y)
+def get_frontier(X, Y, nn):
+  '''extracts the envelope via snowball method'''
+  radius = get_curvature(X, Y, nn)
   n = len(X)
   idx1 = 0
   idx2 = 1
   envelope_X = [X[0]]
   envelope_Y = [Y[0]]
   while idx2 < n:
+    r = radius((X[idx1] + X[idx2]) / 2)
     xc, yc = get_circle(X[idx1], Y[idx1], X[idx2], Y[idx2], r)
     empty = True
     for i in range(idx2 + 1, n):
@@ -198,75 +356,26 @@ def get_frontier(X, Y):
         idx2 += 1
         break
     if empty:
-      # envelope_X.append(X[idx1]) , envelope_Y.append(Y[idx1])
-      envelope_X.append(X[idx2]) , envelope_Y.append(Y[idx2])
+      envelope_X.append(X[idx2])
+      envelope_Y.append(Y[idx2])
       idx1 = idx2
       idx2 += 1
   return envelope_X, envelope_Y
 
 
-def remove_envelope(X, Y, pulses):
-  W = np.zeros(pulses[-1].start + pulses[-1].len)
-  f = interp1d((X), Y, kind="linear", fill_value='extrapolate', assume_sorted=True)
-  # f = UnivariateSpline(X, Y)
-  for p in pulses:
-    x0 = p.start
-    x1 = p.start + p.len
-    Xn = p.start + np.arange(p.len)
-    A = f(Xn)
-    W[x0 : x1] = p.W / A
-  return W, f
-
-
-def smooth(X, Y, n):
-  Yn = np.zeros(len(Y))
-
-  '''first value'''
-  s = Y[0] + Y[1] + Y[2]
-  h0, h1 = Y[0] / s, Y[1] / s
-
-  d = X[2] - X[0]
-  d0, d1 = 1, 1 - (X[1] - X[0]) / d
-
-  Yn[0] = (Y[0] * h0 * d0 + Y[1] * h1 * d1) / (h0 * d0 + h1 * d1)
-
-  '''last value''' 
-  s = Y[-2] + Y[-1]
-  h0, h1 = Y[-2] / s, Y[-1] / s
-
-  d = X[-1] - X[-3]
-  d0, d1 = 1, 1 - (X[-1] - X[-2]) / d
-
-  Yn[-1] = (Y[-2] * h0 * d0 + Y[-1] * h1 * d1) / (h0 * d0 + h1 * d1)
-  
-  for i in range(1, len(Y) - 1):
-    y0, y, y1 = Y[i - 1], Y[i], Y[i + 1]
-
-    d = X[i + 1] - X[i - 1]
-    d0 = 1 - (X[i] - X[i - 1]) / d
-    d1 = 1 - (X[i + 1] - X[i]) / d
-
-    s = y0 + y + y1
-    h = y / s
-    h0 = y0 / s
-    h1 = y1 / s
-
-    Yn[i] = (y * h + d0 * y0 * h0 + d1 * y1 * h1) / (h + h0 * d0 + h1 * d1)
-
-  f = UnivariateSpline(X, Yn, s=0)
-  return f(np.arange(n))
-
 
 '''==============='''
 ''' Read wav file '''
 '''==============='''
+fig = go.Figure()
 
-name = "tom"
+name = "piano33"
 W, fps = read_wav(f"Samples/{name}.wav")
 W = W - np.average(W)
 amplitude = np.max(np.abs(W))
 W = W / amplitude
 n = W.size
+print(n)
 X = np.arange(n)
 
 # W = W [ : W.size // 10]
@@ -281,16 +390,23 @@ pos_X, neg_X = np.array([p.x for p in pos_pulses]), np.array([p.x for p in neg_p
 pos_Y, neg_Y = np.array([p.y for p in pos_pulses]), np.array([p.y for p in neg_pulses])
 pos_L, neg_L = np.array([p.len for p in pos_pulses]) , np.array([p.len for p in neg_pulses])
 
-pos_env_X, pos_env_Y = get_frontier(pos_X, pos_Y)
+pos_frontier_X, pos_frontier_Y = get_frontier(pos_X, pos_Y, n)
+neg_frontier_X, neg_frontier_Y = get_frontier(neg_X, neg_Y, n)
 
-neg_env_X, neg_env_Y = get_frontier(neg_X, neg_Y)
+# pos_env_X, pos_env_Y = smooth(pos_frontier_X, pos_frontier_Y, n)
+# neg_env_X, neg_env_Y = smooth(neg_frontier_X, neg_frontier_Y, n)
 
+# XX, YY, II = get_knots_indices(pos_frontier_X, pos_frontier_Y, 2)
+
+# A = constrained_least_squares_arbitrary_intervals(pos_frontier_X, pos_frontier_Y, II, k=3)
+# YY = coefs_to_array_arbitrary_intervals(A, X, II, n)
+# XX = X
 
 '''============================================================================'''
 '''                                    PLOT                                    '''
 '''============================================================================'''
 
-fig = go.Figure()
+
 fig.layout.template ="plotly_white"
 # fig.update_yaxes(zeroline=True, zerolinewidth=2, zerolinecolor="black")
 fig.update_layout(
@@ -321,7 +437,8 @@ fig.add_trace(
         # size=8,
         color="silver",
         # showscale=False
-    )
+    ),
+    visible = "legendonly"
   )
 )
 
@@ -347,7 +464,8 @@ fig.add_trace(
     y=areas_Y,
     fill="tozeroy",
     mode="none",
-    fillcolor="rgba(0,0,0,0.16)"
+    fillcolor="rgba(0,0,0,0.16)",
+    visible = "legendonly"
   )
 )
 
@@ -362,7 +480,8 @@ fig.add_trace(
         size=6,
         color="black",
         # showscale=False
-    )
+    ),
+    visible = "legendonly"
   )
 )
 
@@ -377,130 +496,72 @@ fig.add_trace(
         size=6,
         color="black",
         # showscale=False
-    )
+    ),
+    visible = "legendonly"
   )
 )
 
 fig.add_trace(
   go.Scatter(
     name="+ Frontier",
-    x=pos_env_X,
-    y=pos_env_Y,
+    x=pos_frontier_X,
+    y=pos_frontier_Y,
     # fill="toself",
-    mode="lines+markers",
+    mode="lines",
     line=dict(
         width=1,
         color="red",
         # showscale=False
-    )
+    ),
+    visible = "legendonly"
   )
 )
 
 fig.add_trace(
   go.Scatter(
     name="- Frontier",
-    x=neg_env_X,
-    y=neg_env_Y,
-    # fill="toself",
-    mode="lines+markers",
-    line=dict(
-        width=1,
-        color="red",
-        # showscale=False
-    )
-  )
-)
-
-YY = smooth(pos_env_X, pos_env_Y, n)
-
-
-fig.add_trace(
-  go.Scatter(
-    name="YY",
-    x=X,
-    y=YY,
+    x=neg_frontier_X,
+    y=neg_frontier_Y,
     # fill="toself",
     mode="lines",
     line=dict(
         width=1,
-        color="black",
+        color="red",
         # showscale=False
-    )
+    ),
+    visible = "legendonly"
   )
 )
 
 # fig.add_trace(
 #   go.Scatter(
-#     name="Ye0",
-#     x=Xs,
-#     y=Ye0,
+#     name="Knots",
+#     x=np.array([[pos_frontier_X[i], pos_frontier_X[i], None] for i in II]).flat,
+#     y=np.array([[-1, 1, None] for _ in II]).flat,
 #     # fill="toself",
 #     mode="lines",
 #     line=dict(
 #         width=1,
-#         color="blue",
+#         color="black",
 #         # showscale=False
-#     )
+#     ),
+#     visible = "legendonly"
 #   )
 # )
 
 # fig.add_trace(
 #   go.Scatter(
-#     name="Ye1",
-#     x=Xs,
-#     y=Ye1,
+#     name="Pos Envelope Avgs",
+#     x=XX,
+#     y=YY,
 #     # fill="toself",
 #     mode="lines",
 #     line=dict(
-#         width=1,
+#         # width=1,
 #         color="blue",
 #         # showscale=False
-#     )
-#   )
-# )
-
-# fig.add_trace(
-#   go.Scatter(
-#     name="Yo0",
-#     x=Xs,
-#     y=Yo0,
-#     # fill="toself",
-#     mode="lines",
-#     line=dict(
-#         width=1,
-#         color="blue",
-#         # showscale=False
-#     )
-#   )
-# )
-
-# fig.add_trace(
-#   go.Scatter(
-#     name="Yo1",
-#     x=Xs,
-#     y=Yo1,
-#     # fill="toself",
-#     mode="lines",
-#     line=dict(
-#         width=1,
-#         color="blue",
-#         # showscale=False
-#     )
-#   )
-# )
-
-# fig.add_trace(
-#   go.Scatter(
-#     name="Negative Envelope Cubic",
-#     x=X,
-#     y=savgol_filter(f_neg(X), 101, 3),
-#     # fill="toself",
-#     mode="lines",
-#     line=dict(
-#         width=1,
-#         color="blue",
-#         # showscale=False
-#     )
+#     ),
+#     visible = "legendonly"
 #   )
 # )
 
