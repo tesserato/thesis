dc2b3a365e657a16b3ac7cb53c12cd2748abe665
tesserato <tesserato@hotmail.com>
2020-10-09 14:36:41 -0300

    updated rule in latex workshop

--- Text/00_build.ps1
+++ Text/00_build.ps1
@@ -1,10 +1,10 @@
 # latexmk -lualatex -f -interaction=nonstopmode THESIS.tex
 
 $name = "THESIS"
-lualatex.exe $name -f -interaction=nonstopmode -file-line-error
-biber.exe $name
-makeglossaries.exe $name
-lualatex.exe $name -f -interaction=nonstopmode -file-line-error
-lualatex.exe $name -f -interaction=nonstopmode -file-line-error
+lualatex $name -f -interaction=nonstopmode -file-line-error
+biber $name
+makeglossaries $name
+lualatex $name -f -interaction=nonstopmode -file-line-error
+lualatex $name -f -interaction=nonstopmode -file-line-error
 
 # pandoc PulsesEnvelope.tex --bibliography=bibli.bib -o PulsesEnvelope.docx
